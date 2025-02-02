d64368b6364c9145ccd64b8c651744fdbc4c61d3
Uvoi <iovu_dmtr@yahoo.com>
2025-02-02 20:20:06 +0300

    feat(backend/endpoints/product): Добавление ендпоинта для получения мерок. Обновление функции добавления нового товара

--- backend/endpoints/product.py
+++ backend/endpoints/product.py
@@ -1,37 +1,55 @@
 from uuid import UUID
-from fastapi import APIRouter
-from data_base import getAllProducts, addNewProduct, getProductByID, getCompositionByID
-from pydantic import BaseModel
+from fastapi import APIRouter, HTTPException
+from data_base import getAllProducts, add_product, getProductByID, get_measurement_names, add_required_measurements
+from pydantic import BaseModel, Field
+from typing import List, Optional
 
 
 router = APIRouter(prefix="/api/product")
 
 
-class Product(BaseModel):
-    id: int
-    name: str
-    description: str
-    price: float
-    weight: float
-    diameter: float
-    imglink: str
-    composition: str
+class ProductCreate(BaseModel):
+    name: str = Field(..., max_length=100)
+    description: str = ""
+    price: float = Field(..., gt=0)
+    category: str = Field(..., max_length=50)
+    imglink: str = ""
+    composition: str = ""
+    gender: str = Field(..., pattern="^(male|female|unisex)$")
+    required_measurements: List[str] = []
+
+
+
+@router.post("/")
+async def create_product(product: ProductCreate):
+    try:
+        new_product = add_product(
+            name=product.name,
+            description=product.description,
+            price=product.price,
+            gender=product.gender,
+            category=product.category,
+            imglink=product.imglink,
+            composition=product.composition
+        )
+        add_required_measurements(new_product.productid, product.required_measurements)
+
+        return {"message": "Product successfully created", "product": new_product}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error adding product: {str(e)}")
+
 
 @router.get("/")
 async def catalog_all():
     return getAllProducts()
 
-@router.post("/")
-async def catalog_add(addNewRowData: Product):
-    addNewProduct(addNewRowData.name, addNewRowData.description, addNewRowData.price, addNewRowData.weight, addNewRowData.diameter, addNewRowData.imglink, addNewRowData.composition, addNewRowData.id)
-    return addNewRowData
+@router.get("/measurements")
+async def get_measurements():
+    return get_measurement_names()
 
 
 @router.get("/{product_id}")
 async def asteroid_by_id(product_id: int):
     product_info = getProductByID(product_id)
-    composition_info = getCompositionByID(product_id)
-    combined_info = {**product_info, **composition_info}
+    combined_info = {**product_info}
     return combined_info
-
-
