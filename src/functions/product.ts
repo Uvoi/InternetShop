a2554be637a83267d0a0d5b028db89e6ca9575ef
Uvoi <iovu_dmtr@yahoo.com>
2025-02-15 09:59:55 +0300

    feat(functions/product): обновление интерфейса продукта; добавление функции получения мерок

--- src/functions/product.ts
+++ src/functions/product.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-export interface UserData {
-  id: number,
-  name: string,
-  description: string,
-  price: number,
-  weight: number,
-  diameter: number,
-  imglink: string,
-  composition: string,
+export interface ProdData {
+  name: string;
+  description: string;
+  price: Number;
+  category: string;
+  imglink: string;
+  composition: string;
+  gender: string;
+  required_measurements: string[];
 }
 
 export async function deleteProduct(id:number) {
@@ -24,7 +24,7 @@ export async function deleteProduct(id:number) {
     }
 }
 
-export async function addNewProduct(data:UserData) {
+export async function addNewProduct(data:ProdData) {
     try 
     {
         const response = await axios.post('http://127.0.0.1:8000/api/product', data, {withCredentials: true});
@@ -49,14 +49,26 @@ export async function getProduct(id:number) {
   }
   
 
-  export async function getCatalog () {
-    try {
-      const response = await axios.get('http://127.0.0.1:8000/api/product/', {
-        withCredentials: true,
-      });
-      return response.data;
-    } catch (error) {
-      console.error('Ошибка при обновлении данных:', error);
-      throw error;
-    }
-  };
+export async function getCatalog () {
+  try {
+    const response = await axios.get('http://127.0.0.1:8000/api/product/', {
+      withCredentials: true,
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Ошибка при обновлении данных:', error);
+    throw error;
+  }
+};
+
+export async function getMeasurements () {
+  try {
+    const response = await axios.get('http://127.0.0.1:8000/api/product/measurements', {
+      withCredentials: true,
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Ошибка при получении мерок:', error);
+    throw error;
+  }
+};
